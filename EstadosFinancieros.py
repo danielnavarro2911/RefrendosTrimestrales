0381f314185a95adf41e1f8c8e506648edf01876
Daniel Navarro Quiros <96166574+danielnavarro2911@users.noreply.github.com>
2025-06-04 11:23:51 -0600

    Update EstadosFinancieros.py

--- EstadosFinancieros.py
+++ EstadosFinancieros.py
@@ -162,8 +162,12 @@ class EstadosFinancieros:
         df['Cuenta'] = df['Cuenta'].str.replace(r'\b\d+\.[a-zA-Z]{2}\b', '', regex=True).str.strip()
         df['Cuenta'] = df['Cuenta'].str.replace(r'/', '').str.strip()
         df['Cuenta'] = df['Cuenta'].str.replace('(','').str.replace(')','').str.replace(',','').str.strip()
-        try: df.columns = ['Cuenta','Monto Actual','Trimestre Anterior','Anio Pasado']
-        except: df.columns = ['Cuenta','Monto Actual','Anio Pasado']
+        try:
+            #df = df.iloc[:,[0,1,2,3]] 
+            df.columns = ['Cuenta','Monto Actual','Trimestre Anterior','Anio Pasado']
+        except: 
+            df = df.iloc[:,[0,1,2]]
+            df.columns = ['Cuenta','Monto Actual','Anio Pasado']
         df['Total']=df.iloc[:, 1:].sum(axis=1)
 
         return df
